c425be1b238157663085406a22831188e3639e3c
NedaHeydarii <heydarin596@gmail.com>
2025-04-20 13:24:06 -0700

    responsive

--- src/components/panel/CoursePage/CoursePage.jsx
+++ src/components/panel/CoursePage/CoursePage.jsx
@@ -37,25 +37,35 @@ import { DeleteCourse } from "../../../assets/icons/studentPanel/DeleteCourse";
         <div className=" flex flex-col px-11 pt-15 rtl gap-6 w-full 
         ">
 
-            <div className="flex gap-36 bg-[#6033FE] text-white  text-center py-4 w-full px-9  whitespace-nowrap rounded-[10px]
+            <div className="flex gap-36 bg-[#6033FE] text-white  text-center py-4 w-full px-9  whitespace-nowrap rounded-[10px] 
             max-lg:gap-5 max-md:text-xs">
+                   <p> مدیریت</p>
+                   <p>قیمت</p>
+                   <p>تاریخ شروع</p>
+                   <p>مدرس</p>
+                   <p> نام دوره</p>
                 <p>تصویر</p>
-                <p> نام دوره</p>
-                <p>مدرس</p>
-                <p>تاریخ شروع</p>
-                <p>قیمت</p>
-                <p> مدیریت</p>
+              
+               
+               
+                
+             
 
             </div>
             {myCourse.map((data,ind) =>(
                       <div key={ind} className="flex gap-32.5 text-center py-3.5 w-full px-10 whitespace-nowrap rounded-[10px] bg-gray-200 justify-center leading-8
                       max-lg:gap-5 max-md:text-xs max-md:gap-4">
+                             <DeleteCourse/>
+                             <p>{data.coursePrice}</p>
+                             <p>{data.startDate}</p>
+                             <p>{data.courseTeacher}</p>
+                             <p> {data.courseType}</p>
                          <div className="border-2 border-black w-6 h-6 rounded-[4px]"></div>
-                         <p> {data.courseType}</p>
-                         <p>{data.courseTeacher}</p>
-                         <p>{data.startDate}</p>
-                         <p>{data.coursePrice}</p>
-                         <DeleteCourse/>
+                      
+                    
+                      
+                     
+                    
          
                      </div>
             ))}
