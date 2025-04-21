e68158e7cfe1e73759f637e7251b0ceb9d6fcb1c
NedaHeydarii <heydarin596@gmail.com>
2025-04-20 22:38:28 -0700

    darkmode

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -2,13 +2,36 @@ import { Logo } from "../../../assets/fonts/icons/header/Logo"
 import { Shop } from "../../../assets/fonts/icons/header/Shop"
 
 const Header = ()=>{
+
+    const [darkMode, setDarkMode] = useState(() => {  
+      
+        const storedMode = localStorage.getItem('dark-mode');  
+        return storedMode === 'true';  
+      });  
+    
+      const DarkMode = () => {  
+        const newDarkMode = !darkMode;  
+        setDarkMode(newDarkMode);  
+        localStorage.setItem('dark-mode', newDarkMode);
+      };  
+    
+      
+      useEffect(() => {  
+        if (darkMode) {  
+          document.documentElement.classList.add('dark');  
+        } else {  
+          document.documentElement.classList.remove('dark');  
+        }  
+      }, [darkMode]);  
+
+
     return(
         <div className=" w-full h-15 px-20 flex  items-center justify-between
           max-md:whitespace-nowrap max-md:mt-2">
             <div className=" h-8 flex gap-1 pt-2 
             max-md:-mr-20">
                <Logo/>
-                <p className="text-xl tracking-tight leading-9 max-md:text-sm">هگزا اسکواد</p>
+                <p className={`text-xl tracking-tight leading-9 max-md:text-sm ${darkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>هگزا اسکواد</p>
             </div>
             <div className="w-86.5 h-8 flex gap-10 text-base font-medium tracking-tight whitespace-nowrap leading-13 mr-25
             max-md:text-[12px] max-md:gap-3 max-md:mx-6">
@@ -19,11 +42,11 @@ const Header = ()=>{
             </div>
             <div className="w-50 h-12.5 flex justify-between gap-4 
             max-md:gap-1 max-md:">
-                <div className="w-12 h-12 rounded-4xl bg-[#FFFF]  relative  ]
-               max-md:w-9 max-md:h-9 max-md:px-1.5 max-md:py-1 max-md:mt-1.5">
+                <button onClick={DarkMode} className="w-12 h-12 rounded-4xl bg-[#FFFF]  relative  
+               max-md:w-9 max-md:h-9 max-md:px-1.5 max-md:py-1 max-md:mt-1.5" >{darkMode ? 'Light Mode' : 'Dark Mode'} 
                 <img className="" src="public/header/4458120.png"/>
                    
-                </div>
+                </button>
                 <div className=" w-33 h-12 bg-[#2196F3] rounded-[80px]
                 max-md:w-17 max-md:h-8 max-md:rounded-3xl max-md:mt-1.5 ">
                     <p className="text-base font-bold text-white text-center leading-12
