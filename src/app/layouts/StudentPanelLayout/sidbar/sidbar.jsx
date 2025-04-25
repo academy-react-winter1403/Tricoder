4205f8dbeb3c4243985684d2f71b4c74ce10bfd7
phatemeh amiri <fatemehamiri978@gmail.com>
2025-04-25 18:19:50 +0330

    edit student panel

--- src/app/layouts/StudentPanelLayout/sidbar/sidbar.jsx
+++ src/app/layouts/StudentPanelLayout/sidbar/sidbar.jsx
@@ -1,23 +1,20 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { FiX } from "react-icons/fi";
 import { useSelector, useDispatch } from "react-redux";
 import { closeMenu } from "../../../../redux/Store/menuSlice";
 import { BookOpenCheck, BookOpenText, CalendarHeart, FileHeart, KeyRound, LayoutDashboard, LucideUserCircle2, MessageCircleMore, Power,} from "lucide-react";
-import { User2Icon } from "lucide-react";
-import { FaUserCircle } from "react-icons/fa";
-// import { LayoutDashboard } from "lucide-react";
-// import { LayoutDashboard } from "lucide-react";
-// import { LayoutDashboard } from "lucide-react";
+
 
 const Sidebar = () => {
   const itsOpen = useSelector((state) => state.menu.isOpen);
   const manuDispatch = useDispatch();
 
+ 
   return (
     <>
       <div
-        className={`fixed top-0  w-[20rem]  h-[100%] border-solid   bg-gradient-to-b from-purple-500 to-indigo-500 
-         md:block md:static  md:h-[98%]  md:w-[20%]   md:rounded-2xl  -right-full   transition-all duration-1000    md:text-sm   lg:text-lg
+        className={`fixed top-0  w-[20rem]  h-[100%]   md:h-[150vh]   lg:h-[100%] border-solid   bg-gradient-to-b from-purple-500 to-indigo-500 
+         md:block md:static  md:h-[98%]  md:w-[23%]   md:rounded-2xl  -right-full   transition-all duration-1000    md:text-xs   lg:text-base
          ${itsOpen ? "right-0" : "-right-full"}
                 `}
       >
@@ -26,7 +23,7 @@ const Sidebar = () => {
             className="text-white  m-3  md:hidden "
             onClick={() => manuDispatch(closeMenu())}
           >
-            <FiX size={30} />
+            <FiX size={30}    />
           </button>
         )}
         {console.log("itsOpen:", itsOpen)}
--- src/redux/Store/store.js
+++ src/redux/Store/store.js
@@ -2,11 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import menuReducer from './menuSlice'
 import coursetabReducer from './courseDetailTabs'
-
+import profileReducer from './profileSlice';
 
 export const store = configureStore({
     reducer: {
         menu:menuReducer,
         coursetab:coursetabReducer,
+        profile:profileReducer,
     },
 })
