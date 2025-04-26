ac8e1af973958035a0b4023c47f1a5694089d397
NedaHeydarii <heydarin596@gmail.com>
2025-04-26 15:51:06 -0700

    news

--- src/components/newsDetail/viewNewsDetail/NewsView.jsx
+++ src/components/newsDetail/viewNewsDetail/NewsView.jsx
@@ -8,7 +8,7 @@ const NewsView = ({ detailsNewsDto }) => {
   return (
     <div className='flex px-20 pt-14 gap-8 
     max-lg:flex-col'>
-      <div className="border-2 rounded-[5px] w-[30%] ">
+      <div className="border-2 rounded-[5px] w-fit h-[40%] ">
         <img className='relative' src={(detailsNewsDto.currentImageAddress) ? detailsNewsDto.currentImageAddress : "/newsDetail/notfound.jpg"} />
 
       </div>
@@ -16,7 +16,7 @@ const NewsView = ({ detailsNewsDto }) => {
         .inUsersFavoriteCount}</p></div>
       <div className='flex flex-col '>
         <p className='text-[32px]  font-bold'>{detailsNewsDto.title}</p>
-        <p className='font-normal text-[#455A64] text-base leading-7'>{detailsNewsDto.describe}</p>
+        <p className='font-normal text-[#455A64] text-base leading-7 w-fit'>{detailsNewsDto.describe}</p>
 
         <div className='flex justify-between  pt-4 items-center'>
 
@@ -25,6 +25,7 @@ const NewsView = ({ detailsNewsDto }) => {
               <EyeIcon />
               <p className='text-sm text-[#2196F3] '>{detailsNewsDto.currentView}</p>
             </div>
+
 
             <div className='w-1.5 h-1.5 rounded-[80px] bg-[#2196F3] mt-2'></div>
 
