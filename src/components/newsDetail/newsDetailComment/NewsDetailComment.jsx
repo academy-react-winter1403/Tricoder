c60258116bd3112ddfe794c28d00a0ed0563577c
NedaHeydarii <heydarin596@gmail.com>
2025-04-30 23:13:38 -0700

    commentsss

--- src/components/newsDetail/newsDetailComment/NewsDetailComment.jsx
+++ src/components/newsDetail/newsDetailComment/NewsDetailComment.jsx
@@ -30,7 +30,7 @@ const NewsDetailComment = () => {
         userIpAddress: "192.11.86.20",
         title: "عباس قلی",
         describe: "سلام جالب نبود",
-        userId: "40581"
+        userId: 40581
       }
     })
     
@@ -51,21 +51,21 @@ const NewsDetailComment = () => {
       
                      <div className='flex justify-between '>
                      <div className='flex gap-2'>
-                        <div className=' border-2 rounded-3xl w-8 h-8'></div>
-                        <p className='font-medium leading-8 text-base'>محمد زمانی</p>
+                        <div className=' border-2 rounded-3xl w-8 h-8'>{data.pictureAddress}</div>
+                        <p className='font-medium leading-8 text-base'>{data.autor} </p>
                       </div>
                       <div className='text-[#607D8B] text-xs font-normal leading-8'>2 روز پیش</div>
                      </div>
         
-                      <p className='text-sm text-[#455A64]'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون.</p>
+                      <p className='text-sm text-[#455A64]'>{data.describe}</p>
                      
                       <div className='flex gap-3'>
                         <div className='flex gap-1'>
-                          <p className='text-[#F44336] text-sm font-bold'>7</p>
+                          <p className='text-[#F44336] text-sm font-bold'>{data.likeCount}</p>
                           <NewsCommentLike/>
                         </div>
                         <div className='flex gap-1'>
-                          <p className='text-[#455A64] text-sm font-normal'>پاسخ</p>
+                          <p className='text-[#455A64] text-sm font-normal'> {data.replyCount}  پاسخ  </p>
                           <CommentReply/>
                         </div>
                       </div>
